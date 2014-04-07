4c21c5fe70ce782129c73360a307b6bab8b91a67
Anthony Scatchell <Scatchell@gmail.com>
2014-04-07 16:45:02 +0200

    First commit (too late)

---
+++ discovery-rss-reader/rss.rb
@@ -0,0 +1,25 @@
+require 'rss'
+require 'open-uri'
+
+class RSSHealth
+    URL = 'http://dhdb2bbld02.discsrv.co.za:8080/job/B2B-hospital-auths-mediclinic-outgoing-develop/rssAll'
+
+    def self.broken?
+        open(URL) do |rss|
+            feed = RSS::Parser.parse(rss)
+
+            return nil if feed.items.nil?
+            return feed.items.first.title.to_s.include? 'broken'
+        end
+    end
+
+    def self.build_num
+        open(URL) do |rss|
+            feed = RSS::Parser.parse(rss)
+            return nil if feed.nil? || feed.items.nil?
+
+            title = feed.items.first.title.to_s
+            return title.slice(title.index('#') + 1, 4)
+        end
+    end
+end
