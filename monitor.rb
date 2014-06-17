3f17ff828017404ed5244b684d6947f77a8b44c8
Anthony Scatchell <Scatchell@gmail.com>
2014-06-17 16:04:57 +0200

    Renaming main file and adding url's to be used

--- monitor.rb
+++ monitor.rb
@@ -4,9 +4,10 @@ last_time = Time.new
 
 last_build_num = '0'
 
-while true
-    current_time = Time.new
+GREEN_URL='http://www.traveltop.net/wp-content/uploads/2012/01/golden-mountain-lake-calm-forest-golden-lake-mountain.jpg'
+RED_URL='http://hdwallsdesk.com/wp-content/uploads/2013/08/explosion_background.jpg'
 
+while true
     begin
         if(!RSSHealth.build_num.nil? && RSSHealth.build_num != last_build_num)
             build_num = RSSHealth.build_num
