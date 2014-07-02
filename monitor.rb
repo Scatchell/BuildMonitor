540cc63233d26497fa007fa6972c3335c3f82717
Anthony Scatchell <ascatche@thoughtworks.com>
2014-07-02 15:22:15 +0200

    Adding build is better sound and some logic to avoid playing every time there is a good checkin

--- monitor.rb
+++ monitor.rb
@@ -7,6 +7,9 @@ last_build_num = '0'
 GREEN_URL='http://www.traveltop.net/wp-content/uploads/2012/01/golden-mountain-lake-calm-forest-golden-lake-mountain.jpg'
 RED_URL='http://www.zingerbug.com/Backgrounds/background_images/airstrip_explosions.jpg'
 
+first = true
+last_state = nil
+
 while true
 	begin
 		if(!RSSHealth.build_num.nil? && RSSHealth.build_num != last_build_num)
@@ -16,9 +19,17 @@ while true
 				`firefox #{RED_URL}`
 				`play ~/Downloads/fail-trombone-03.wav`
 			else
-				`firefox #{GREEN_URL}`
+				if last_state != RSSHealth.broken?
+					`firefox #{GREEN_URL}`
+					if !first
+						`play ~/Downloads/smw_castle_clear.wav`
+					else
+						first  = false
+					end
+				end
 			end
 
+			last_state = RSSHealth.broken?
 			last_build_num = build_num
 		end
 	rescue SocketError
